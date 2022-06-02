eaeacaac8a82df6add753b19cafa2b36fda8babb
Graciela Parente <graciparentte@gmail.com>
2022-06-02 15:10:34 -0300

    feat: create config folder with mongodb connection

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+	path: process.env.NODE_ENV,
+});
+
+module.exports = {
+	database: {
+		host: process.env.DB_HOST,		
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		collection: process.env.DB_COLLECTION,
+	}
+};
