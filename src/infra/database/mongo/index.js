85d459612e2c927a561d4771a09f2254dd5b658b
Graciela Parente <graciparentte@gmail.com>
2022-06-02 15:11:12 -0300

    feat: create mongoose connection with mongodb

---
+++ src/infra/database/mongo/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('../../../config/config');
+
+class Database {
+	constructor() {
+		this.connect();
+	}
+
+	connect(){
+		return mongoose.connect(
+			`mongodb://${config.database.host}/${config.database.collection}`,
+		);
+	}
+}
+
+module.exports = new Database().connect();
