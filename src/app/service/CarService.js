da4afd8234bb8cbd56e2d87f7986c5bc3457c65b
Graciela Parente <graciparentte@gmail.com>
2022-06-01 23:17:07 -0300

    feat: create car service

---
+++ src/app/service/CarService.js
@@ -0,0 +1,12 @@
+const CarRepository = require('../repository/CarRepository');
+
+class CarService {
+
+	async create(payload) {
+		const result = await CarRepository.create(payload);
+
+		return result;
+	}
+}
+
+module.exports = new CarService();
