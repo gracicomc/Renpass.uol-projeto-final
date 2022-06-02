3bf6ded268bf890a418717f0f8cf87f667dc7085
Graciela Parente <graciparentte@gmail.com>
2022-06-01 22:53:17 -0300

    feat: create car routes file

---
+++ src/app/routes/car.router.js
@@ -0,0 +1,11 @@
+const CarController = require('');
+
+module.exports = (server, routes, prefix='/api/v1/car') => {
+	routes.post('/', CarController.create);
+	routes.get('/', CarController.list);
+	routes.get('/:id', CarController.getById);
+	routes.put('/:id', CarController.update);
+	routes.delete('/:id', CarController.deleteById);
+
+	server.use(prefix, routes);
+};
