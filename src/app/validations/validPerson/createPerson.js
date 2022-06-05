1ad11530ac25a94ecb873ac5de436f37e25bee55
Graciela Parente <graciparentte@gmail.com>
2022-06-05 15:59:28 -0300

    feat: create validations with JOI to create and update person

--- src/app/validations/validPerson/createPerson.js
+++ src/app/validations/validPerson/createPerson.js
@@ -1,15 +1,15 @@
 const Joi = require('joi')
 	.extend(require('@joi/date'));
+const { cpf } = require('../../utils/regex');
 const validCPF = require('../../utils/validCPF');
-const email = require('../../utils/regex');
 
 module.exports = async (req, res, next) => {
 	try {
 		const schemaPerson = Joi.object({
 			name: Joi.string().min(3).max(30).required().trim(),
-			cpf: Joi.string().required(),
-			birthDay: Joi.date().required().format('DD/MM/YYYY').max('01/01/2004'),
-			email: Joi.string().min(10).required().email(),
+			cpf: Joi.string().required().regex(cpf).message('Invalid character'),
+			birthDay: Joi.date().required().format('DD/MM/YYYY').min('01/01/2004'),
+			email: Joi.string().min(10).required().email().lowercase().trim(),
 			password: Joi.string().min(6).required(),
 			canDrive: Joi.string().required().valid('yes', 'no')
             
@@ -18,8 +18,9 @@ module.exports = async (req, res, next) => {
 		const { error } = await schemaPerson.validate(req.body, { abortEarl: true });
 
 		if (error) throw error;
-		if (!validCPF(req.body.cpf)) throw error;
-		if(!email(req.body.email)) throw error;
+
+		if(!validCPF(req.body.cpf)) throw {message: 'Invalid CPF'};
+
 		return next();
 	} catch (error) {
 		return res.status(400).json({Error: error.message});
