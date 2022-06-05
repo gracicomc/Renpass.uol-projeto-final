31cc2d384a4976f56126b06ec491d09caa92e193
Graciela Parente <graciparentte@gmail.com>
2022-06-05 00:54:50 -0300

    feat: create validations for car schema

---
+++ src/app/validations/validCar/patchCar.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports = async (req, res, next) => {
+	try {
+		const schemaCar = Joi.object({
+			model: Joi.string().min(3).required().trim(), 
+			type: Joi.string().min(3).required().trim(),
+			brand: Joi.string().min(2).required().trim(),
+			color: Joi.string().min(2).required().trim(),
+			year: Joi.number().min(1950).max(2022).required(),
+			accessories: Joi.array().min(1).unique().required()
+				.items({description: Joi.string().min(1).required().trim()}), 
+			passengersQtd: Joi.number().required().min(1)
+		});
+		const { error } = schemaCar.validate(req.body, { abortEarly: false});
+		if (error) throw error;
+		return next();
+	} catch (error) {
+		return res.status(400).json({Error: error.message});
+	}
+};
--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,7 +1,8 @@
 const CarController = require('../app/controller/CarController');
+const createCar = require('../app/validations/validCar/createCar');
 
 module.exports = (server, routes, prefix='/api/v1/car') => {
-	routes.post('/', CarController.create);
+	routes.post('/', createCar, CarController.create);
 	routes.get('/', CarController.list);
 	routes.get('/:id', CarController.getById);
 	routes.patch('/:id', CarController.patchCar);
