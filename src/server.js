6fefd8162ce55071e9b28f11c6d568bdc9a381f3
Graciela Parente <graciparentte@gmail.com>
2022-06-07 11:34:54 -0300

    feat: create HTTP request logger with morgan

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+const morgan = require('morgan');
 const app = require('./app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Running API on PORT ${PORT}`);
 });
+
+app.use(morgan('dev'));
