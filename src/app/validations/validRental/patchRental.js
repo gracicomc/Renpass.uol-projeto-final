d5eda793c5bbda6081037a5af09676da414fe0da
Graciela Parente <graciparentte@gmail.com>
2022-06-14 15:37:39 -0300

    feat: create patch validation for rental

---
+++ src/app/validations/validRental/patchRental.js
@@ -0,0 +1,39 @@
+const Joi = require('joi')
+	.extend(require('@joi/date'));
+const { cnpj } = require('../../utils/regex');
+const validCNPJ = require('../../utils/validCNPJ');
+
+module.exports = async (req, res, next) => {
+	try {
+		const schemaRental = Joi.object({
+			name: Joi.string().min(3).max(50)
+				.trim(),
+			cnpj: Joi.string().regex(cnpj).message('Invalid character in CNPJ field. Try something like: 00.000.000/0000-00'),
+			activities: Joi.string().max(100).trim(),
+			address: Joi.array().items(Joi.object({
+				zipCode: Joi.string().trim(),
+				street: Joi.string().max(100).trim(),
+				complement: Joi.string().max(100).trim(),
+				number: Joi.string().trim(),
+				district: Joi.string().max(100).trim(),
+				city: Joi.string().max(100).trim(),
+				state: Joi.string().max(3).trim(),
+				isFilial: Joi.boolean(),
+			})),
+		});
+
+		const { error } = await schemaRental.validate(req.body, { abortEarl: true });
+
+		if (error) throw error;
+
+		if (!validCNPJ(req.body.cnpj)) throw { message: 'Invalid CNPJ' };
+
+		return next();
+	} catch (error) {
+		return res.status(400).json({
+			statusCode: error.statusCode,
+			description: error.description,
+			error: error.message,
+		});
+	}
+};
