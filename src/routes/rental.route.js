ff01bdc58ea682eaa9ac474e88f15b8f1db0b072
Graciela Parente <graciparentte@gmail.com>
2022-06-14 15:50:10 -0300

    feat: call validations in rental post and patch routes

--- src/routes/rental.route.js
+++ src/routes/rental.route.js
@@ -1,7 +1,9 @@
 const RentalController = require('../app/controller/RentalController');
+const createRental = require('../app/validations/validRental/createRental');
+const patchRental = require('../app/validations/validRental/patchRental');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
-	routes.post('/', RentalController.create);
+	routes.post('/', createRental, RentalController.create);
 	routes.get('/', RentalController.list);
 	routes.get('/:id', RentalController.getById);
 	routes.patch('/:id', RentalController.patchRental);
