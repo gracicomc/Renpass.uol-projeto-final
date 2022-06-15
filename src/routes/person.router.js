ff2e5e2df3c61149cc24949ccd1b747f6da4ad25
Graciela Parente <graciparentte@gmail.com>
2022-06-15 16:05:22 -0300

    fix: remove authentication from person and rental routes

--- src/routes/person.router.js
+++ src/routes/person.router.js
@@ -1,13 +1,12 @@
 const PersonController = require('../app/controller/PersonController');
 const createPerson = require('../app/validations/validPerson/createPerson');
-const authMiddleware = require('../app/middlewares/auth');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
 	routes.post('/', createPerson, PersonController.create);
-	routes.get('/', authMiddleware, PersonController.list);
-	routes.get('/:id', authMiddleware, PersonController.getById);
-	routes.patch('/:id', authMiddleware, PersonController.patchPerson);
-	routes.delete('/:id', authMiddleware, PersonController.deletePerson);
+	routes.get('/', PersonController.list);
+	routes.get('/:id', PersonController.getById);
+	routes.patch('/:id', PersonController.patchPerson);
+	routes.delete('/:id', PersonController.deletePerson);
 
 	server.use(prefix, routes);
 };
--- src/routes/rental.route.js
+++ src/routes/rental.route.js
@@ -1,10 +1,8 @@
 const RentalController = require('../app/controller/RentalController');
 const createRental = require('../app/validations/validRental/createRental');
 const patchRental = require('../app/validations/validRental/patchRental');
-const authMiddleware = require('../app/middlewares/auth');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
-	routes.use(authMiddleware);
 	routes.post('/', createRental, RentalController.create);
 	routes.get('/', RentalController.list);
 	routes.get('/:id', RentalController.getById);
