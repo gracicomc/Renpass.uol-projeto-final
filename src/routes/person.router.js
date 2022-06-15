61e8dd516f709f18425012f5f5e04cf75ce0ed13
Graciela Parente <graciparentte@gmail.com>
2022-06-15 01:31:49 -0300

    fix: adjust authentication in person routes

--- src/routes/person.router.js
+++ src/routes/person.router.js
@@ -3,12 +3,11 @@ const createPerson = require('../app/validations/validPerson/createPerson');
 const authMiddleware = require('../app/middlewares/auth');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
-	routes.use(authMiddleware);
 	routes.post('/', createPerson, PersonController.create);
-	routes.get('/', PersonController.list);
-	routes.get('/:id', PersonController.getById);
-	routes.patch('/:id', PersonController.patchPerson);
-	routes.delete('/:id', PersonController.deletePerson);
+	routes.get('/', authMiddleware, PersonController.list);
+	routes.get('/:id', authMiddleware, PersonController.getById);
+	routes.patch('/:id', authMiddleware, PersonController.patchPerson);
+	routes.delete('/:id', authMiddleware, PersonController.deletePerson);
 
 	server.use(prefix, routes);
 };
