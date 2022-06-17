21141461488b1e17b02fea92af274f8e4a8160cf
Graciela Parente <graciparentte@gmail.com>
2022-06-17 08:31:15 -0300

    feat: create Fleet Schema

---
+++ src/app/schema/FleetSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const FleetSchema = new mongoose.Schema({
+  id_car: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Car',
+    required: true,
+  },
+  id_rental: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Rental',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['available', 'unavailable', 'rented'],
+    required: true,
+  },
+  daily_value: {
+    type: Number,
+    required: true,
+  },
+  plate: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+FleetSchema.plugin(mongoosePaginate);
+
+const Fleet = mongoose.model('Fleet', FleetSchema);
+module.exports = Fleet;
