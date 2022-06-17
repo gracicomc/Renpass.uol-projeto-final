a89054d6d9165874bcff0b4bdfe252ea04ff4543
Graciela Parente <graciparentte@gmail.com>
2022-06-17 08:31:32 -0300

    feat: create Reserve Schema

---
+++ src/app/schema/ReserveSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const ReserveSchema = new mongoose.Schema({
+  id_user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Person',
+    required: true,
+  },
+  data_start: {
+    type: String,
+    required: true,
+  },
+  data_end: {
+    type: String,
+    required: true,
+  },
+  id_car: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Car',
+    required: true,
+  },
+  id_rental: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Rental',
+    required: true,
+  },
+  final_value: {
+    type: Number,
+    required: true,
+  },
+});
+
+ReserveSchema.plugin(mongoosePaginate);
+
+const Reserve = mongoose.model('Reserve', ReserveSchema);
+module.exports = Reserve;
