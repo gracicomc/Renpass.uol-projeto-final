342eb34c3865cd09224bbddc39d36a0b4269e3ca
Graciela Parente <graciparentte@gmail.com>
2022-06-19 23:59:09 -0300

    fix: remove eslint configs

--- src/app/utils/validCNPJ.js
+++ src/app/utils/validCNPJ.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable no-undef */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-plusplus */
@@ -5,23 +6,23 @@
 function validCNPJ(cnpj) {
   cnpj = cnpj.replace(/[^\d]+/g, '');
 
-  if (cnpj === '') return false;
+  if (cnpj == '') return false;
 
   if (cnpj.length !== 14) {
     return false;
   }
 
   if (
-    cnpj === '00000000000000' ||
-    cnpj === '11111111111111' ||
-    cnpj === '22222222222222' ||
-    cnpj === '33333333333333' ||
-    cnpj === '44444444444444' ||
-    cnpj === '55555555555555' ||
-    cnpj === '66666666666666' ||
-    cnpj === '77777777777777' ||
-    cnpj === '88888888888888' ||
-    cnpj === '99999999999999'
+    cnpj == '00000000000000' ||
+    cnpj == '11111111111111' ||
+    cnpj == '22222222222222' ||
+    cnpj == '33333333333333' ||
+    cnpj == '44444444444444' ||
+    cnpj == '55555555555555' ||
+    cnpj == '66666666666666' ||
+    cnpj == '77777777777777' ||
+    cnpj == '88888888888888' ||
+    cnpj == '99999999999999'
   ) {
     return false;
   }
@@ -38,7 +39,7 @@ function validCNPJ(cnpj) {
     }
   }
   result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-  if (result !== digits.charAt(0)) {
+  if (result != digits.charAt(0)) {
     return false;
   }
 
@@ -53,7 +54,7 @@ function validCNPJ(cnpj) {
     }
   }
   result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-  if (result !== digits.charAt(1)) {
+  if (result != digits.charAt(1)) {
     return false;
   }
 
