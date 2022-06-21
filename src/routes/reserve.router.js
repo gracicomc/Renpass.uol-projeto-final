8201f72fbf2d8c21041aefbabc1bca6794f67c32
Graciela Parente <graciparentte@gmail.com>
2022-06-21 14:03:25 -0300

    feat: add validations to routes

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,7 +1,8 @@
 const ReserveController = require('../app/controller/ReserveController');
+const createReserve = require('../app/validations/validReserve/createReserve');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
-  routes.post('/:rentalId/reserve/', ReserveController.create);
+  routes.post('/:rentalId/reserve/', createReserve, ReserveController.create);
   routes.get('/:rentalId/reserve/', ReserveController.list);
   routes.get('/:rentalId/reserve/:id', ReserveController.getById);
   routes.patch('/:rentalId/reserve/:id', ReserveController.patchReserve);
