91174682ae3d1b8a51a101eb9c849220ba6fcb50
Graciela Parente <graciparentte@gmail.com>
2022-06-21 09:56:56 -0300

    fix: add disconnect

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -11,6 +11,10 @@ class Database {
       `mongodb+srv://admin:PDrk684TOHcAW7Wf@renpass.vebld.mongodb.net/?retryWrites=true&w=majority`
     );
   }
+
+  disconnect() {
+    return mongoose.connection.close();
+  }
 }
 
 module.exports = new Database().connect();
