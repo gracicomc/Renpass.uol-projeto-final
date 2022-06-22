0125540ec633a9da2bd3a6655bae81bd29867161
Graciela Parente <graciparentte@gmail.com>
2022-06-22 02:40:07 -0300

    fix: adjust delete route

--- src/app/utils/Errors/carErrors/NotUniqAcessorie.js
+++
@@ -1,9 +0,0 @@
-class NotUniqAcessorie extends Error {
-	constructor() {
-		super();
-		this.statusCode = 400;
-		this.description = 'Bad Request';
-		this.message = 'Acessories contains a duplicate value';
-	}
-}
-module.exports = NotUniqAcessorie;
--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -6,7 +6,7 @@ module.exports = (server, routes, prefix = '/api/v1/rental') => {
   routes.get('/:rentalId/reserve/', ReserveController.list);
   routes.get('/:rentalId/reserve/:id', ReserveController.getById);
   routes.patch('/:rentalId/reserve/:id', ReserveController.patchReserve);
-  routes.delete('/:rentalId/reserve:id', ReserveController.deleteReserve);
+  routes.delete('/:rentalId/reserve/:id', ReserveController.deleteReserve);
 
   server.use(prefix, routes);
 };
