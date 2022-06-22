475faf9fc91d59ae562f35dde4f1d5c48b4e27d8
Graciela Parente <graciparentte@gmail.com>
2022-06-22 02:42:29 -0300

    commit

---
+++ src/app/validations/validReserve/patchReserve.js
@@ -0,0 +1,36 @@
+const Joi = require('joi').extend(require('@joi/date'));
+const { id } = require('../../utils/regex');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schemaReserve = Joi.object({
+      id_user: Joi.string()
+        .regex(id)
+        .message('Invalid character in id_user field'),
+      date_start: Joi.date().format('DD/MM/YYYY'),
+      date_end: Joi.date().format('DD/MM/YYYY'),
+      id_car: Joi.string()
+        .regex(id)
+        .message('Invalid character in id_car field'),
+      id_rental: Joi.string()
+        .regex(id)
+        .message('Invalid character in id_rental field'),
+      final_value: Joi.number().min(1),
+    });
+
+    const { error } = await schemaReserve.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (error) throw error;
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      invalidFields: error.details.map((detail) => ({
+        field: detail.path.join('.'),
+        description: detail.message,
+      })),
+    });
+  }
+};
