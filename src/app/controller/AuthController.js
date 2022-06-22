03c2f9f904beb87e2a42c3876d788088bfbef98d
Graciela Parente <graciparentte@gmail.com>
2022-06-22 08:46:45 -0300

    style: eslint changes

--- src/app/controller/AuthController.js
+++ src/app/controller/AuthController.js
@@ -1,19 +1,19 @@
 const AuthService = require('../service/AuthService');
 
 class AuthController {
-	async authenticate(req, res) {
-		try {
-			const { email, password } = await req.body;
-			const result = await AuthService.authenticate(email, password);
+  async authenticate(req, res) {
+    try {
+      const { email, password } = await req.body;
+      const result = await AuthService.authenticate(email, password);
 
-			return res.status(201).json(result);
-		} catch (error) {
-			return res.status(400).json({
-				statusCode: error.statusCode,
-				description: error.description,
-				error: error.message,
-			});
-		}
-	}
+      return res.status(201).json(result);
+    } catch (error) {
+      return res.status(400).json({
+        statusCode: error.statusCode,
+        description: error.description,
+        error: error.message,
+      });
+    }
+  }
 }
 module.exports = new AuthController();
