5a0791014165651e0dc3e1ff110e14d041e7b9b7
Graciela Parente <graciparentte@gmail.com>
2022-06-22 11:14:31 -0300

    feat: add authentication to car routes

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,13 +1,13 @@
 const CarController = require('../app/controller/CarController');
 const createCar = require('../app/validations/validCar/createCar');
 const patchCar = require('../app/validations/validCar/patchCar');
-// const authMiddleware = require('../app/middlewares/auth');
+const authMiddleware = require('../app/middlewares/auth');
 const getCar = require('../app/validations/validCar/getCar');
 const deleteCar = require('../app/validations/validCar/deleteCar');
 const validId = require('../app/validations/validId');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-  // routes.use(authMiddleware);
+  routes.use(authMiddleware);
   routes.post('/', createCar, CarController.create);
   routes.get('/', getCar, CarController.list);
   routes.get('/:id', validId, getCar, CarController.getById);
