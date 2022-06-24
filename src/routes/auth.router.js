fdb2f7dd061de132e4a2f4c083ddc73b0d6ede72
Graciela Parente <graciparentte@gmail.com>
2022-06-23 21:20:00 -0300

    feat: aply joi validations to auth router

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,7 +1,8 @@
 const AuthController = require('../app/controller/AuthController');
+const validateAuth = require('../app/validations/validAuth/validateAuth');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
-  routes.post('/', AuthController.authenticate);
+  routes.post('/', validateAuth, AuthController.authenticate);
 
   server.use(prefix, routes);
 };
