39d898495d5c0cc640531bc69f81886e2a1fe39b
Graciela Parente <graciparentte@gmail.com>
2022-06-23 21:19:18 -0300

    fic: adjust path for swagger docs file

--- src/routes/api-docs.router.js
+++ src/routes/api-docs.router.js
@@ -1,5 +1,5 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('../swagger.json');
+const swaggerDocs = require('../app/docs/swagger.json');
 
 module.exports = (server, routes, prefix = '/api/v1/api-docs') => {
   routes.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
