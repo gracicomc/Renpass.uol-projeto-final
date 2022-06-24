97280d098019f5e5e4d5c3f85099ed2b60f77ec1
Graciela Parente <graciparentte@gmail.com>
2022-06-23 21:30:40 -0300

    ref: remove unecessary await

--- src/app/controller/AuthController.js
+++ src/app/controller/AuthController.js
@@ -3,12 +3,12 @@ const AuthService = require('../service/AuthService');
 class AuthController {
   async authenticate(req, res) {
     try {
-      const { email, password } = await req.body;
-      const result = await AuthService.authenticate(email, password);
+      const { email, password } = req.body;
+      const result = AuthService.authenticate(email, password);
 
-      return res.status(201).json(result);
+      return res.status(200).json(result);
     } catch (error) {
-      return res.status(400).json({
+      return res.status(401).json({
         statusCode: error.statusCode,
         description: error.description,
         error: error.message,
