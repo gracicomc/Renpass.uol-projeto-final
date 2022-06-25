652ca216d391e4f9828599d3a2e327a6f1b83867
Graciela Parente <graciparentte@gmail.com>
2022-06-25 11:24:44 -0300

    rm: remove delete person validation and put only validId

--- src/routes/person.router.js
+++ src/routes/person.router.js
@@ -2,7 +2,6 @@ const PersonController = require('../app/controller/PersonController');
 const createPerson = require('../app/validations/validPerson/createPerson');
 const patchPerson = require('../app/validations/validPerson/patchPerson');
 const getPerson = require('../app/validations/validPerson/getPerson');
-const deletePerson = require('../app/validations/validPerson/deletePerson');
 const validId = require('../app/validations/validId');
 
 module.exports = (server, routes, prefix = '/api/v1/person') => {
@@ -10,7 +9,7 @@ module.exports = (server, routes, prefix = '/api/v1/person') => {
   routes.get('/', getPerson, PersonController.list);
   routes.get('/:id', validId, getPerson, PersonController.getById);
   routes.patch('/:id', validId, patchPerson, PersonController.updateById);
-  routes.delete('/:id', validId, deletePerson, PersonController.deleteById);
+  routes.delete('/:id', validId, PersonController.deleteById);
 
   server.use(prefix, routes);
 };
