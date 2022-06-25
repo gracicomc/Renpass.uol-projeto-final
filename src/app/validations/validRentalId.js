68782e2ddebee57062851c7437cb33d3f87aa6b1
Graciela Parente <graciparentte@gmail.com>
2022-06-25 11:44:56 -0300

    feat: create validation for rentalID

---
+++ src/app/validations/validRentalId.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const { id } = require('../utils/regex');
+
+module.exports = async (req, res, next) => {
+  try {
+    const validId = Joi.object({
+      rentalId: Joi.string().regex(id).message('Invalid character in ID parameter.').required()
+    });
+
+    const { error } = await validId.validate(req.params, {
+      abortEarly: false
+    });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(error.statusCode || 400).json({
+      invalidId: error.details.map((detail) => ({
+        parameter: detail.path.join('.'),
+        description: detail.message
+      }))
+    });
+  }
+};
