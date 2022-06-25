64e88c5f0126a1a87b5dce9b2c3d549a6c04046b
Graciela Parente <graciparentte@gmail.com>
2022-06-25 11:19:35 -0300

    feat: create validation for accessories ID

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,8 +3,8 @@ const createCar = require('../app/validations/validCar/createCar');
 const patchCar = require('../app/validations/validCar/patchCar');
 // const authMiddleware = require('../app/middlewares/auth');
 const getCar = require('../app/validations/validCar/getCar');
-const deleteCar = require('../app/validations/validCar/deleteCar');
 const validId = require('../app/validations/validId');
+const validAcessoryId = require('../app/validations/validAcessoryId');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
   // routes.use(authMiddleware);
@@ -12,8 +12,8 @@ module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.get('/', getCar, CarController.list);
   routes.get('/:id', validId, getCar, CarController.getById);
   routes.patch('/:id', patchCar, CarController.updateById);
-  routes.patch('/:id/accessories/:accessoriesId', validId, CarController.patchAccessories);
-  routes.delete('/:id', validId, deleteCar, CarController.deleteById);
+  routes.patch('/:id/accessories/:accessoriesId', validId, validAcessoryId, CarController.patchAccessories);
+  routes.delete('/:id', validId, CarController.deleteById);
 
   server.use(prefix, routes);
 };
