6395e33eb69692bdaea69af32b2e56a0f47ea5ab
Graciela Parente <graciparentte@gmail.com>
2022-06-26 22:35:25 -0300

    feat: create validations for update reserve

--- src/app/service/ReserveService.js
+++ src/app/service/ReserveService.js
@@ -12,7 +12,71 @@ class ReserveService {
     if (!rental) throw new NotFound(`This Rental ID '${rentalId}' is not registered`);
     payload.id_rental = rentalId;
 
-    const { date_start, date_end, id_car } = payload;
+    const { date_start, date_end, id_car, id_user } = payload;
+
+    const car = await FleetRepository.list({ id_car });
+    if (!car) throw new NotFound(`The Car ID ${id_car} is not registered.`);
+
+    const user = await PersonRepository.getById(id_user);
+    if (!user) throw new NotFound(`The User ID '${id_user}' is not registered`);
+
+    if (user.canDrive === 'no') throw new BadRequest(`The user must have a driver's license`);
+
+    const searchBookedCar = await ReserveRepository.list({
+      date_start,
+      date_end,
+      id_car
+    });
+    if (searchBookedCar.length > 0) {
+      throw new BadRequest(`This Car is already booked. It was booked on ${date_start} - ${date_end}`);
+    }
+
+    const validDataStart = moment(date_start, 'DD/MM/YYYY').isSameOrBefore(moment(date_end, 'DD/MM/YYYY'), 'days');
+    const validDataEnd = moment(date_end, 'DD/MM/YYYY').isSameOrAfter(moment(date_start, 'DD/MM/YYYY'), 'days');
+    if (!validDataStart && !validDataEnd)
+      throw new BadRequest(`Invalid input date. Data Start can't be bigger then Data End`);
+
+    const fleet = await FleetRepository.list({ id_car });
+    const getDailyValue = fleet[0].daily_value;
+    const bookedDays = moment(date_end, 'DD/MM/YYYY').diff(moment(date_start, 'DD/MM/YYYY'), 'days');
+    payload.final_value = getDailyValue * bookedDays;
+
+    const result = await ReserveRepository.create(payload);
+    if (!result) throw new BadRequest();
+    return result;
+  }
+
+  async list(payload) {
+    const result = await ReserveRepository.list(payload);
+    if (!result) throw new BadRequest();
+    return result;
+  }
+
+  async getById(rentalId, payload) {
+    const rental = await RentalRepository.getById(rentalId);
+    if (!rental) throw new NotFound(`This Rental ID '${rentalId}' is not registered`);
+
+    const result = await ReserveRepository.getById(payload);
+    if (!result) throw new NotFound(`The Reserve ID ${payload} is not registered`);
+    return result;
+  }
+
+  async updateById(rentalId, id, payload) {
+    const { date_start, date_end, id_car, id_user } = payload;
+    const rental = await RentalRepository.getById(rentalId);
+    if (!rental) throw new NotFound(`This Rental ID '${rentalId}' is not registered`);
+    payload.id_rental = rentalId;
+
+    if (payload.id_car) {
+      const car = await FleetRepository.list({ id_car });
+      if (!car) throw new NotFound(`The Car ID ${id_car} is not registered.`);
+    }
+
+    if (payload.id_user) {
+      const user = await PersonRepository.getById(id_user);
+      if (!user) throw new NotFound(`The User ID '${id_user}' is not registered`);
+      if (user.canDrive === 'no') throw new BadRequest(`The user must have a driver's license`);
+    }
 
     const searchBookedCar = await ReserveRepository.list({
       date_start,
@@ -31,39 +95,17 @@ class ReserveService {
     const getFleet = await FleetRepository.list({ id_car });
     const getDailyValue = getFleet[0].daily_value;
     const bookedDays = moment(date_end, 'DD/MM/YYYY').diff(moment(date_start, 'DD/MM/YYYY'), 'days');
-
     payload.final_value = getDailyValue * bookedDays;
 
-    const { id_user } = payload;
-    const user = await PersonRepository.getById(id_user);
-    if (!user) throw new NotFound(`The User ID '${id_user}' is not registered`);
-
-    if (user.canDrive === 'no') throw new BadRequest(`The user must have a driver's license`);
-
-    const result = await ReserveRepository.create(payload);
-    if (!result) throw new BadRequest();
-    return result;
-  }
-
-  async list(payload) {
-    const result = await ReserveRepository.list(payload);
-    if (!result) throw new BadRequest();
-    return result;
-  }
-
-  async getById(payload) {
-    const result = await ReserveRepository.getById(payload);
-    if (!result) throw new NotFound(`The Reserve ID ${payload} is not registered`);
-    return result;
-  }
-
-  async updateById(id, payload) {
     const result = await ReserveRepository.updateById(id, payload);
     if (!result) throw new NotFound(`The Reserve ID ${payload} is not registered`);
     return result;
   }
 
-  async deleteById(payload) {
+  async deleteById(rentalId, payload) {
+    const rental = await RentalRepository.getById(rentalId);
+    if (!rental) throw new NotFound(`This Rental ID '${rentalId}' is not registered`);
+
     const result = await ReserveRepository.deleteById(payload);
     if (!result) throw new NotFound(`The Reserve ID ${payload} is not registered`);
     return result;
